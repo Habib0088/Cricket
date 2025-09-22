98d015166e446c121795e5fcb65fae6e8b7ba313
Habib0088 <sarkerbatch2k19@gmail.com>
2025-09-22 15:24:39 +0600

    card created

--- src/assets/component/players/Playerscard.jsx
+++ src/assets/component/players/Playerscard.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React from "react";
 
-const Playerscard = ({item}) => {
-    // {name}=item
-    
-    return (
-        <div>
-            
+const Playerscard = ({ item }) => {
+  let {
+    name,
+    image,
+    price,
+    country,
+    role,
+    battingStyle,
+    rating,
+    bowlingStyle,
+  } = item;
+
+  return (
+    <div className="">
+      <div className="card bg-base-100 shadow-sm p-4 rounded-md h-full">
+        <figure>
+          <img
+            className="rounded-xl mb-4"
+            src={image}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <span>😎</span>
+          <h2 className="card-title font-bold inline ml-2">{name}</h2>
+          <div className="flex justify-between border-b-2 border-gray-100 py-2">
+            <div className="flex  items-center">
+              <img
+                className="mr-2 w-[20px] h-[20px]"
+                src="../../../../public/report.jpg"
+                alt=""
+              />
+              <span>{country}</span>
+            </div>
+            <button className="bg-gray-300 px-2 py-1 rounded-md ">
+              {role}
+            </button>
+          </div>
+
+          <div className="rating flex justify-between">
+            <span className="font-bold"> Rating</span>
+            <small>{rating}</small>
+          </div>
+          <div className="style flex justify-between">
+            <span className="font-bold">{battingStyle}</span>
+            <span>{bowlingStyle}</span>
+          </div>
+          <div className="style flex justify-between">
+            <span className="font-bold">
+              Price $ <span>{price}</span>
+            </span>
+            <button className="bg-gray-300 px-2 py-1 rounded-md">
+              Choose Player
+            </button>
+          </div>
+          <div className="card-actions text-end mt-4">
+            <button className="bg-gray-300 px-2 py-1 rounded-md outline outline-gray-400">
+              Buy Now
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Playerscard;
--- src/assets/component/players/Playes.jsx
+++ src/assets/component/players/Playes.jsx
@@ -1,46 +1,21 @@
 import React, { use } from "react";
 import Playerscard from "./Playerscard";
 
-import hear from "/"
+
 
 const Playes = ({ jsonData }) => {
   let data = use(jsonData);
   console.log(data);
 
   return (
-    <div>
-      {data.map((item) => (
+    <div className="max-w-[1250px] m-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {data.map((item) => (
         <Playerscard item={item}></Playerscard>
       ))}
-
-      <div className="card bg-base-100 w-96 shadow-sm p-4">
-        <figure>
-          <img className="rounded-xl mb-4"
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-            <span>😎</span>
-          <h2 className="card-title font-bold inline ml-2">Virat Kholi</h2>
-          <div className="flex justify-between">
-                <div>
-                    <img className="w-[50px] h-[50px] bg-black mr-7" src={'./user 1.jpg'} alt="" />
-                    <span>India</span>
-                </div>
-                <button className="btn">All-Rounder</button>
-          </div>
-          
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
       </div>
+
+    
     </div>
   );
 };
