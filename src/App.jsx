9bbd2e9376bdb21024aaf9c3c42dd72c2ef885f0
Habib0088 <sarkerbatch2k19@gmail.com>
2025-09-22 11:59:23 +0600

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import coin from "./assets/coin.png";
+import logo from "./assets/logo.png";
 
 function App() {
-  return <>
-<button className="btn">Default</button>
-
-  </>;
+  return (
+    <>
+      <div className="navbar flex justify-between items-center py-4 max-w-[1250px] m-auto">
+        {/* Left */}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        {/* right */}
+        <div className="flex items-center justify-between">
+          <span className="mr-2">600000</span>
+          <span className="mr-2">coin</span>
+          <span><img className="w-[20px] h-[20px]" src={coin} alt="" /></span>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
